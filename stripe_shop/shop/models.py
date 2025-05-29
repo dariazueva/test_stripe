49de6014662ad63a597b22668c306aec5c0d8779
Daria <DZueva26@gmail.com>
2025-05-29 12:05:04 +0500

    add docker

--- stripe_shop/shop/models.py
+++ stripe_shop/shop/models.py
@@ -2,17 +2,13 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField("Наименование", max_length=255)
-    description = models.TextField("Описание")
-    price = models.IntegerField("Цена", help_text="в центах")
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField(help_text="Price in cents")
 
     @property
     def display_price(self):
         return "{:.2f}".format(self.price / 100)
-
-    class Meta:
-        verbose_name = "Товар"
-        verbose_name_plural = "Товары"
 
     def __str__(self):
         return self.name
