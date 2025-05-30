c30c95a285870a32c08dccda466bc4d644e1b722
Daria <DZueva26@gmail.com>
2025-05-30 09:57:31 +0500

    fix

--- stripe_shop/stripe_shop/settings.py
+++ stripe_shop/stripe_shop/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["test-stripe-635h.onrender.com"]
 
 
 # Application definition
